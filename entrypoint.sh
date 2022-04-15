72150d8727184358910c04f665ebe7837429eb72
Mark D. Gray <markdaltongray@gmail.com>
2022-04-15 16:47:18 -0500

    more testing

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,6 @@ set -exu
 if [ ! -r "$BASELINE_FILE" ]; then
 
     if [ "$REQUIRE_BASELINE" -eq 0 ]; then
-        echo "Hello world"
         detect-secrets scan >"$BASELINE_FILE"
     else
         echo "No readable detect-secrets baseline file found at '$BASELINE_FILE', and it was set to required by \$DS_REQUIRE_BASELINE ($REQUIRE_BASELINE)" >&2
@@ -15,5 +14,5 @@ if [ ! -r "$BASELINE_FILE" ]; then
 
 fi
 
-#detect-secrets -v audit "$BASELINE_FILE"
+detect-secrets -v audit "$BASELINE_FILE"
 detect-secrets scan #"$ADDL_ARGS"
